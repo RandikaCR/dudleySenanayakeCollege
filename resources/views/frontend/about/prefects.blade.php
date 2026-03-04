911ccd8876d1cabf6f33603ebb29f8fc168b402b
Randika De Alwis <randika@motordat.com>
2026-03-04 13:59:45 +0530

    changes

--- resources/views/frontend/about/prefects.blade.php
+++ resources/views/frontend/about/prefects.blade.php
@@ -25,6 +25,7 @@
         <div class="container">
             <div class="row justify-content-center mb_70">
                 <div class="col-sm-12 mb-5 text-center">
+                    <h2 class="mb-4 fw-bold">Prefect Guild - 2026</h2>
                     <h3 class="fw-bold text-primary">Head Prefects</h3>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
@@ -32,8 +33,8 @@
                         <figure class="image_box">
                             <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
                         </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                        <h4><a href="#">MALKA RASAANJANA</a></h4>
+                        <p>Head Prefect</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
@@ -41,22 +42,22 @@
                         <figure class="image_box">
                             <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
                         </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                        <h4><a href="#">MALSHI AMAYA</a></h4>
+                        <p>Head Prefect</p>
                     </div>
                 </div>
             </div>
             <div class="row justify-content-center mb_70">
                 <div class="col-sm-12 mb-5 text-center">
-                    <h3 class="fw-bold text-primary">Senior Prefects</h3>
+                    <h3 class="fw-bold text-primary">Games Captains</h3>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
                     <div class="career_feature_block centred">
                         <figure class="image_box">
                             <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
                         </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                        <h4><a href="#">MINARU NIMTHARU</a></h4>
+                        <p>Games Captain</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
@@ -64,32 +65,23 @@
                         <figure class="image_box">
                             <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
                         </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
-                    <div class="career_feature_block centred">
-                        <figure class="image_box">
-                            <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
-                        </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                        <h4><a href="#">RANUGI DAHAMSA</a></h4>
+                        <p>Games Captain</p>
                     </div>
                 </div>
             </div>
 
             <div class="row justify-content-center mb_70">
                 <div class="col-sm-12 mb-5 text-center">
-                    <h3 class="fw-bold text-primary">Junior Prefects</h3>
+                    <h3 class="fw-bold text-primary">Deputy Head Prefects</h3>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
                     <div class="career_feature_block centred">
                         <figure class="image_box">
                             <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
                         </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                        <h4><a href="#">NIJAL AKASH</a></h4>
+                        <p>Deputy Head Prefect</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
@@ -97,47 +89,11 @@
                         <figure class="image_box">
                             <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
                         </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
+                        <h4><a href="#">ASHIKAA</a></h4>
+                        <p>Deputy Head Prefect</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
-                    <div class="career_feature_block centred">
-                        <figure class="image_box">
-                            <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
-                        </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-                    </div>
-                </div>
-
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
-                    <div class="career_feature_block centred">
-                        <figure class="image_box">
-                            <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
-                        </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
-                    <div class="career_feature_block centred">
-                        <figure class="image_box">
-                            <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
-                        </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-5">
-                    <div class="career_feature_block centred">
-                        <figure class="image_box">
-                            <a href="#"><img src="{{ asset('assets/common/images/user-square.jpg') }}" alt=""></a>
-                        </figure>
-                        <h4><a href="#">Name Goes here</a></h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
-                    </div>
-                </div>
+     
             </div>
         </div>
     </section>
